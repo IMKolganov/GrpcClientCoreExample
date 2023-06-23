927da7fa499462abddc24e6fffc217cc943c6fae
Ivan <110064602+IMKolganov@users.noreply.github.com>
2023-06-23 18:07:02 +0300

    add call service

--- Program.cs
+++ Program.cs
@@ -10,18 +10,25 @@ Console.WriteLine("Start!");
 
 try
 {
-    Console.WriteLine($"Create chanell");
-// создаем канал для обмена сообщениями с сервером
-// параметр - адрес сервера gRPC
-    using var channel = GrpcChannel.ForAddress("http://localhost:5063");
-// создаем клиент
-    var client = new Greeter.GreeterClient(channel);
-    Console.Write("Enter name: ");
-    var name = Console.ReadLine();
-// обмениваемся сообщениями с сервером
-    var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
-    Console.WriteLine($"Response server: {reply.Message}");
-    Console.ReadKey();
+    for (int i = 0; i < 10; i++)
+    {
+
+
+        Console.WriteLine($"Create chanell");
+        
+        // создаем канал для обмена сообщениями с сервером
+        // параметр - адрес сервера gRPC
+        using var channel = GrpcChannel.ForAddress("https://localhost:7154/");
+        // создаем клиент
+        var client = new Greeter.GreeterClient(channel);
+        // Console.Write("Enter name: ");
+        var name = "alla";
+        Console.ReadLine();
+        // обмениваемся сообщениями с сервером
+        var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+        Console.WriteLine($"Response server: {reply.Message}");
+        Console.ReadKey();
+    }
 }
 catch (Exception ex)
 {
